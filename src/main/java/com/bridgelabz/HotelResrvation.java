b714fd5da154d5456c25cae99342fadc6cadcfc6
vanna2020 <vannasingh1999@gmail.com>
2021-10-11 19:49:32 -0700

    Welcome Message

---
+++ src/main/java/com/bridgelabz/HotelResrvation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class HotelResrvation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Reservation Program in HotelReservation class");
+    }
+}
